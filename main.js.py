c78fb756f4bfa7ad092d147872dbc5ac8de6ce84
rodrigoburg <rodrigo.burgarelli1@gmail.com>
2015-06-15 14:22:37 -0300

    dados prontos para analise

--- main.js.py
+++ main.js.py
@@ -8,6 +8,9 @@ import csv
 from pygeocoder import Geocoder
 import json
 import time
+from pandas import DataFrame, read_csv
+import numpy as np
+import collections
 
 def le_arquivos(mypath):
     return [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
@@ -227,7 +230,9 @@ def acha_indices(headers):
     return saida
 
 def acha_coordenadas_bairros():
-    with open("dados_imoveis/dados_filtrados.json","r") as jsonfile:
+    #with open("dados_imoveis/dados_filtrados.json","r") as jsonfile:
+    with open("dados_imoveis/dados_bairro_com_geo.json","r") as jsonfile:
+
         dados = json.load(jsonfile)
 
     Capital = {}
@@ -261,7 +266,6 @@ def acha_coordenadas_bairros():
     Capital["tocantins"] = "palmas"
 
     estados = dict((v,k) for k,v in Capital.items())
-
 
     for capital in dados:
         print("Estamos em: "+capital)
@@ -336,10 +340,107 @@ def acha_media_lista_dict(lista,key):
 
     return saida / tamanho if tamanho > 0 else 0
 
+def exporta_bairros_csvs():
+    with open("dados_imoveis/dados_bairro_com_geo.json","r") as jsonfile:
+        dados = json.load(jsonfile)
+
+    saida = {"cidade":[],"bairro":[],"ano":[],"valor":[],"lat":[],"long":[]}
+    for cidade in dados:
+        for bairro in dados[cidade]:
+            if "2010" in dados[cidade][bairro]:
+                if "coordenadas" not in dados[cidade][bairro]:
+                    print("Bairro sem coordenadas: "+bairro+" - "+cidade)
+                    continue
+
+                saida["cidade"].append(cidade)
+                saida["bairro"].append(bairro)
+                saida["ano"].append(2010)
+                saida["valor"].append(dados[cidade][bairro]["2010"]["valor"])
+                saida["lat"].append(dados[cidade][bairro]["coordenadas"][0])
+                saida["long"].append(dados[cidade][bairro]["coordenadas"][1])
+
+    df = DataFrame(saida)
+
+    df.to_csv("dados_imoveis/dados_total_2010.csv",index=False)
+    #df = df[df.cidade == "sao paulo"]
+    #df.to_csv("dados_imoveis/dados_total_2010_sp.csv",index=False)
+
+def filtra_dados_censo():
+    dados = read_csv("dados_censo/setores_universo.csv")
+    cidades = ["SÃO PAULO","BRASÍLIA","RIO DE JANEIRO","BELO HORIZONTE","PORTO ALEGRE","CURITIBA","SALVADOR","RECIFE","FORTALEZA","BELÉM","MANAUS"]
+    zona_urbana = ["1","2","3"]
+    dados = dados[dados.mun.isin(cidades)]
+    dados = dados[dados.situacao.isin(zona_urbana)]
+    dados.to_csv("dados_censo/setores_universo.csv",index=False)
+
+def acha_indice(setor):
+    setor = dict(setor)
+    variaveis_fora = ["cod_setor","mun","uf","situacao","num_domicilios","renda_total"]
+    saida = []
+    for variavel in setor:
+        if variavel not in variaveis_fora:
+            try:
+                saida.append(int(setor[variavel])/int(setor["num_domicilios"]))
+            except ValueError: #se houver algum erro no valor, como X ou NaN
+                return "incalculavel"
+    return int(np.average(saida)*100)
+
+def calcula_indice_setores():
+    dados = read_csv("dados_censo/setores_universo.csv")
+    dados["indice_urbanismo"] = dados.apply(acha_indice,axis=1)
+    dados = dados[dados.indice_urbanismo != "incalculavel"]
+    dados.to_csv("dados_censo/indice_urbanismo.csv",index=False)
+
+def junta_indice_bairros():
+    dados_indice = read_csv("dados_censo/indice_urbanismo.csv")
+    dados_bairros = read_csv("dados_merged.csv")
+    colunas = ["cod_setor","valor"]
+    for coluna in dados_bairros.columns:
+        if coluna not in colunas:
+            del dados_bairros[coluna]
+
+    dados_bairros.index = dados_bairros["cod_setor"]
+    del dados_bairros["cod_setor"]
+    dados_bairros = dados_bairros.to_dict()
+    dados_bairros = dados_bairros["valor"]
+
+    dados_indice["valor"] = dados_indice["cod_setor"].apply(lambda t:int(dados_bairros[t]) if t in dados_bairros else "erro")
+    dados_indice.to_csv("dados_final_capitais_principais.csv",index=False)
+
+def calcula_desigualdade():
+    setores = read_csv("dados_censo/setores_universo_sp_urbanismo.csv")
+    lista = list(setores["indice_urbanismo"])
+    calculos = collections.OrderedDict()
+    calculos["media_1_superior"] = np.average(np.percentile(lista,99))
+    calculos["media_5_superior"] = np.average(np.percentile(lista,95))
+    calculos["media_10_superior"] = np.average(np.percentile(lista,90))
+    calculos["media_40_inferior"]= np.average(np.percentile(lista,40))
+    calculos["media_10_inferior"] = np.average(np.percentile(lista,10))
+    calculos["gini"] = calcula_gini(lista)
+    calculos["palma"] = calculos["media_10_superior"]/calculos["media_40_inferior"]
+
+    print("INDICE DE URBANIZACAO")
+    for variavel in calculos:
+        print(variavel +": "+str(calculos[variavel]))
+    print()
 
 
+def calcula_gini(list_of_values):
+  sorted_list = sorted(list_of_values)
+  height, area = 0, 0
+  for value in sorted_list:
+    height += value
+    area += height - value / 2.
+  fair_area = height * len(list_of_values) / 2
+  return (fair_area - area) / fair_area
 
 #main_scraper()
 #faz_consulta()
-acha_coordenadas_bairros()
 #filtra_bairros()
+#acha_coordenadas_bairros()
+#exporta_bairros_csvs()
+#filtra_dados_censo()
+#calcula_indice_setores()
+junta_indice_bairros()
+#"CD_GEOCODM" LIKE '%4314902%' or "CD_GEOCODM" LIKE '%4106902%' or "CD_GEOCODM" LIKE '%3550308%' or "CD_GEOCODM" LIKE '%3304557%' or "CD_GEOCODM" LIKE '%2611606%' or "CD_GEOCODM" LIKE '%2927408%' or "CD_GEOCODM" LIKE '%5300108%' or "CD_GEOCODM" LIKE '%3106200%' or "CD_GEOCODM" LIKE '%1302603%' or "CD_GEOCODM" LIKE '%1501402%' or "CD_GEOCODM" LIKE '%2304400%'
+#calcula_desigualdade()
